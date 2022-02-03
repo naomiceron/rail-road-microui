2bd9a9a02ee841078cf7a937910a819f2c2b99b5
ILARA <ilara@EDCMXENLT0809.local>
2022-02-03 13:31:09 -0700

    Agrega comentario

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,18 +1,20 @@
-import React from "react";
-import "../src/styles/App.css";
+import React from 'react';
+import '../src/styles/App.css';
 
 //const Destinations = React.lazy(() => import("remote1/Destinations"));
 // const Receivers = React.lazy(() => import("remote1/Receivers"));
 
-import Destinations from "remote1/Destinations";
-import Receivers from "remote1/Receivers";
-import InputBox from "remote2/InputBox";
+import Destinations from 'remote1/Destinations';
+import Receivers from 'remote1/Receivers';
+import InputBox from 'remote2/InputBox';
 
 // const css = require("../src/styles/App.css");
 
+// Hola
+
 const App = () => (
   <>
-    <div className="App">
+    <div className='App'>
       <h1>HOST</h1>
       <Destinations />
       <Receivers />
